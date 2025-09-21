b925910aeea2bfc6ea88ce7d8224da562eee3e28
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-09-21 21:17:54 +0300

    homework: 2

---
+++ homework_2.py
@@ -0,0 +1,53 @@
+import random
+
+class Car:
+    def __init__(self, trip_distance, model, color):
+        self.fuel = random.randrange(0, 9)
+        self.trip_distance = trip_distance
+        self.model = model
+        self.color = color
+
+
+    def move(self, distance):
+        if self.fuel - distance < 0:
+            self.fuel += random.randrange(0, 9) # дозаправка та пропуск ходу, якщо недостатьно палива для руху на дистанцію
+        else:
+            self.fuel -= distance
+            self.trip_distance += distance
+
+cars = []
+models = ["toyota", "bmw", "audi"]
+colors = ["red", "white", "black"]
+end_race = False
+error = False
+desired_dist = int(input("Відстань до фінішу(повинна бути більше нуля):"))
+
+for number in range(1, 4):
+    car_object = Car(trip_distance= 0, model= models[number - 1], color= colors[number - 1])
+    cars.append(car_object)
+
+race_dist = 0 #int(input("Растояние начала:"))
+# key, car1 in cars_dct.items():
+# print(car1.)
+
+if race_dist >= desired_dist:
+    print("error: Не вірно встановленно відстань до фінішу")
+    error = True
+
+while True:
+    if error:
+        break
+    if end_race:
+        break
+    for car in cars:
+        random_distance = random.randrange(0, 9)
+        car.move(random_distance)
+
+        if car.trip_distance >= desired_dist:
+            print(f"Переможець цього заїзду: {car.model}, дистанцію яку було подоланно: {car.trip_distance}\n")
+            end_race = True
+            break
+if not error:
+    print("Статистика:")
+    for car in cars:
+        print(f"Автомобіль: {car.model}, подоланно дистанцію: {car.trip_distance}, залишилось палива: {car.fuel}")
